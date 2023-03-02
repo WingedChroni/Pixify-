10d940a0475c28758db2ffc5ad73f5101c251399
WingedChroni <Wingedchroni@gmail.com>
2023-03-01 19:44:52 -0500

    organized files into folders

--- js/script.js
+++ js/script.js
@@ -127,33 +127,5 @@ let gallery;
 
 }
 
-// const pTags = document.querySelectorAll("p");
-// const mode = document.querySelector("button.noBorder");
-// const borders = document.querySelectorAll(".button");
-// // console.log(borders);
-// console.log(pTags);
-
-// mode.addEventListener("click", (e)=>{
-//   // alert("working");
-//   document.body.classList.toggle("theme");
-//   // for (border of borders){
-//   //   border.classList.toggle("noBorder");
-//   // }
-  
-//   const pTags = document.querySelectorAll("p");
-//   console.log(document.body.classList.contains("theme"));
-//   if(document.body.classList.contains("theme")){
-//     mode.innerText="Light Mode";
-//     for (p of pTags){
-//       p.style.color ="white";
-//     }
-//   }else{
-//     mode.innerText="Dark Mode";
-//     for (p of pTags){
-//       p.style.color ="black";
-//     }
-//   }
-// })
-
 
 
